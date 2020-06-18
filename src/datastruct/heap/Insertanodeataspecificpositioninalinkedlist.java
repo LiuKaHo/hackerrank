84e2330929c91fca616519e2a31ae5f831a55e16
LiuKaHo <kaho.liu@yahoo.com>
2020-06-18 17:50:25 +0800

    feat:finish head Insertanodeataspecificpositioninalinkedlist

---
+++ src/datastruct/heap/Insertanodeataspecificpositioninalinkedlist.java
@@ -0,0 +1,79 @@
+package datastruct.heap;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/**
+ * @author liukaho
+ * @date 2020/6/18 5:38 PM
+ */
+
+public class Insertanodeataspecificpositioninalinkedlist {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        SinglyLinkedListNode temp = head;
+        for (int i = 0; i < position-1; i++){
+            if (temp.next != null){
+                temp = temp.next;
+            } else {
+                break;
+            }
+        }
+
+        SinglyLinkedListNode next = temp.next;
+        temp.next = new SinglyLinkedListNode(data);
+        temp.next.next = next;
+
+        return head;
+
+    }
+}
