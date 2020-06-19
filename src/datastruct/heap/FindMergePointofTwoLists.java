12e34cb9f3b446bad2f6f9d7a1b25969ed35bc65
LiuKaHo <kaho.liu@yahoo.com>
2020-06-19 15:44:49 +0800

    feat:finish FindMergePointofTwoLists

---
+++ src/datastruct/heap/FindMergePointofTwoLists.java
@@ -0,0 +1,77 @@
+package datastruct.heap;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/**
+ * @author liukaho
+ * @date 2020/6/19 3:14 PM
+ */
+
+public class FindMergePointofTwoLists {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+        while(head1 != null){
+            SinglyLinkedListNode temp = head2;
+            while(temp != null){
+                if (head1 == temp){
+                    return head1.data;
+                }
+                temp = temp.next;
+            }
+            head1 = head1.next;
+        }
+
+        return 0;
+
+    }
+}
