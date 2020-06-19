925da4f67668bb2e1ccaef355997d8a82c997658
LiuKaHo <kaho.liu@yahoo.com>
2020-06-19 16:54:42 +0800

    feat:finish Reverseadoublylinkedlist

---
+++ src/datastruct/heap/Reverseadoublylinkedlist.java
@@ -0,0 +1,85 @@
+package datastruct.heap;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * @author liukaho
+ * @date 2020/6/19 4:13 PM
+ */
+
+public class Reverseadoublylinkedlist {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        if (head == null){
+            return head;
+        }
+
+        DoublyLinkedListNode next = head.next;
+        head.next = head.prev;
+        head.prev = next;
+        if (head.prev != null) {
+            return reverse(head.prev);
+        } else {
+            return head;
+        }
+
+
+
+
+
+    }
+}
